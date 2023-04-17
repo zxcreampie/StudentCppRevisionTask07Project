9d95f77e5dc38506b7369781ad67761fdcebfa92
AlexPronkevich <aa-kk@yandex.ru>
2023-04-17 18:18:09 +0300

    Добавьте файлы проекта.

---
+++ StudentCppRevisionTask07Project/main.cpp
@@ -0,0 +1,8 @@
+#include "tests.h"
+
+int main() {
+	testingTask01();
+	testingTask02();
+	testingTask03();
+	return 0;
+}
---
+++ StudentCppRevisionTask07Project/tasks.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "main.h"
+
+int task01(int, int, int, int);
+string task02(int, int, int);
+bool task03(int, int, int, int, int, int);
